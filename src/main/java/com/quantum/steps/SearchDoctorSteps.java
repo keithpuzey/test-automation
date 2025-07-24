fa742d8624636c1f11a0ca1467577d7b2e94bda6
keith Puzey <keithpuzey@users.noreply.github.com>
2025-07-24 14:27:10 +0100

    Modify Quantum Test with CSV

---
+++ src/main/java/com/quantum/steps/SearchDoctorSteps.java
@@ -0,0 +1,37 @@
+package com.quantum.steps;
+
+import com.qmetry.qaf.automation.ui.WebDriverTestBase;
+import com.qmetry.qaf.automation.ui.webdriver.QAFWebElement;
+import com.qmetry.qaf.automation.ui.webdriver.QAFWebDriver;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+
+public class SearchDoctorSteps {
+
+    @Given("^I navigate to the TopDoctors site$")
+    public void goToTopDoctors() {
+        new WebDriverTestBase().getDriver().get("https://www.topdoctors.co.uk/doctor/");
+    }
+
+    @When("^I search for \"([^\"]*)\"$")
+    public void searchDoctor(String doctorName) {
+        QAFWebDriver driver = new WebDriverTestBase().getDriver();
+
+        QAFWebElement searchInput = driver.findElement("css=input[name='name']");
+        searchInput.clear();
+        searchInput.sendKeys(doctorName);
+
+        // Submit the search (modify if needed to trigger the search)
+        searchInput.sendKeys(Keys.ENTER);
+    }
+
+    @Then("^I wait between searches$")
+    public void waitBetween() {
+        try {
+            Thread.sleep(5000); // 5 second wait
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/resources/scenarios/webSearch.feature
+++ src/main/resources/scenarios/webSearch.feature
@@ -1,10 +1,12 @@
 @Mobile
 Feature: Bing Search
 
-@MobileSearch @retry @TC-1
-Scenario: TC43 - Find a Doctor
-  Given I am on Bing site
-  When I search for "Independent Health Doctor"
-  Then it should have "Doctor" in search results
-  Then I am on Bing Search Page
+  @SearchDoctors
+Feature: TC43 - Find a Doctor
 
+  Scenario Outline: Search doctor name from CSV
+    Given I navigate to the TopDoctors site
+    When I search for "<doctorName>"
+    Then I wait between searches
+
+    Examples: {'datafile' : 'data/doctorNames.csv'
