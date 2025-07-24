5fc5fcfeb97a919279fdf7a06d38be4f129b1424
keithpuzey <keithpuzey@users.noreply.github.com>
2025-07-24 21:49:34 +0100

    Update SearchDoctorSteps.java

--- src/main/java/com/quantum/steps/SearchDoctorSteps.java
+++ src/main/java/com/quantum/steps/SearchDoctorSteps.java
@@ -2,33 +2,32 @@ package com.quantum.steps;
 
 import org.openqa.selenium.Keys;
 
-import com.qmetry.qaf.automation.step.QAFTestStep;
 import com.qmetry.qaf.automation.ui.WebDriverTestBase;
 import com.qmetry.qaf.automation.ui.webdriver.QAFWebElement;
 import com.qmetry.qaf.automation.ui.webdriver.QAFWebDriver;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+
 public class SearchDoctorSteps {
 
-    @QAFTestStep(description = "I navigate to the TopDoctors site")
+    @Given("I navigate to the TopDoctors site")
     public void goToTopDoctors() {
         new WebDriverTestBase().getDriver().get("https://www.topdoctors.co.uk/doctor/");
     }
 
- @QAFTestStep(description = "I search for \"{doctorName}\"")
-public void searchDoctor(String doctorName) {
-    QAFWebDriver driver = new WebDriverTestBase().getDriver();
-
-    // Locate the input field
-    QAFWebElement searchInput = driver.findElement("css=input[aria-label='Specialty / illness / doctor name']");
-    searchInput.clear();
-    searchInput.sendKeys(doctorName);
-
-    // Trigger search
-    searchInput.sendKeys(Keys.ENTER);
+    @When("I search for {string}")
+    public void searchDoctor(String doctorName) {
+        QAFWebDriver driver = new WebDriverTestBase().getDriver();
+        QAFWebElement searchInput = driver.findElement("css=input[aria-label='Specialty / illness / doctor name']");
+        searchInput.clear();
+        searchInput.sendKeys(doctorName);
+        searchInput.sendKeys(Keys.ENTER);
     }
 
-    @QAFTestStep(description = "I wait between searches")
-    public void waitBetween() {
+    @Then("I wait between searches")
+    public void waitBetweenSearches() {
         try {
             Thread.sleep(5000); // wait 5 seconds
         } catch (InterruptedException e) {
