c016b179f543490ffb1deb3a23cd0e963354617d
keith Puzey <keithpuzey@users.noreply.github.com>
2025-07-28 11:18:31 +0100

    Add the given step

--- src/main/java/com/quantum/steps/SearchDoctorSteps.java
+++ src/main/java/com/quantum/steps/SearchDoctorSteps.java
@@ -9,6 +9,11 @@ import com.qmetry.qaf.automation.ui.webdriver.QAFWebDriver;
 
 public class SearchDoctorSteps {
 
+    @QAFTestStep(description = "I navigate to the TopDoctors site")
+    public void goToTopDoctors() {
+        new WebDriverTestBase().getDriver().get("https://www.topdoctors.co.uk/doctor/");
+    }
+
     @QAFTestStep(description = "I search for {0}")
     public void searchDoctor(String doctorName) {
         QAFWebDriver driver = new WebDriverTestBase().getDriver();
