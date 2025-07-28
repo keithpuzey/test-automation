7b925df1a49812edb102d977ed6bd675f3458223
keith Puzey <keithpuzey@users.noreply.github.com>
2025-07-28 11:04:42 +0100

    change to cucumber

--- src/main/java/com/quantum/steps/SearchDoctorSteps.java
+++ src/main/java/com/quantum/steps/SearchDoctorSteps.java
@@ -1,10 +1,9 @@
 package com.quantum.steps;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-
-import com.qmetry.qaf.automation.ui.WebDriverTestBase;
-import com.qmetry.qaf.automation.ui.webdriver.QAFWebElement;
-import com.qmetry.qaf.automation.ui.webdriver.QAFWebDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
@@ -12,15 +11,20 @@ import io.cucumber.java.en.Then;
 
 public class SearchDoctorSteps {
 
+    private WebDriver driver;
+
+    public SearchDoctorSteps(Hooks hooks) {
+        this.driver = hooks.getDriver();
+    }
+
     @Given("I navigate to the TopDoctors site")
     public void goToTopDoctors() {
-        new WebDriverTestBase().getDriver().get("https://www.topdoctors.co.uk/doctor/");
+        driver.get("https://www.topdoctors.co.uk/doctor/");
     }
 
     @When("I search for {string}")
     public void searchDoctor(String doctorName) {
-        QAFWebDriver driver = new WebDriverTestBase().getDriver();
-        QAFWebElement searchInput = driver.findElement("css=input[aria-label='Specialty / illness / doctor name']");
+        WebElement searchInput = driver.findElement(By.cssSelector("input[aria-label='Specialty / illness / doctor name']"));
         searchInput.clear();
         searchInput.sendKeys(doctorName);
         searchInput.sendKeys(Keys.ENTER);
