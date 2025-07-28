3c4eda5919fc573da135a65b079749b2c3a715b7
keith Puzey <keithpuzey@users.noreply.github.com>
2025-07-28 11:14:12 +0100

    Reconfigure Quantum test

--- src/main/java/com/quantum/steps/SearchDoctorSteps.java
+++ src/main/java/com/quantum/steps/SearchDoctorSteps.java
@@ -1,39 +1,27 @@
 package com.quantum.steps;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.When;
-import io.cucumber.java.en.Then;
+import com.qmetry.qaf.automation.step.QAFTestStep;
+import com.qmetry.qaf.automation.ui.WebDriverTestBase;
+import com.qmetry.qaf.automation.ui.webdriver.QAFWebElement;
+import com.qmetry.qaf.automation.ui.webdriver.QAFWebDriver;
 
 public class SearchDoctorSteps {
 
-    private WebDriver driver;
-
-    public SearchDoctorSteps(Hooks hooks) {
-        this.driver = hooks.getDriver();
-    }
-
-    @Given("I navigate to the TopDoctors site")
-    public void goToTopDoctors() {
-        driver.get("https://www.topdoctors.co.uk/doctor/");
-    }
-
-    @When("I search for {string}")
+    @QAFTestStep(description = "I search for {0}")
     public void searchDoctor(String doctorName) {
-        WebElement searchInput = driver.findElement(By.cssSelector("input[aria-label='Specialty / illness / doctor name']"));
+        QAFWebDriver driver = new WebDriverTestBase().getDriver();
+        QAFWebElement searchInput = driver.findElement("css=input[aria-label='Specialty / illness / doctor name']");
         searchInput.clear();
         searchInput.sendKeys(doctorName);
         searchInput.sendKeys(Keys.ENTER);
     }
 
-    @Then("I wait between searches")
+    @QAFTestStep(description = "I wait between searches")
     public void waitBetweenSearches() {
         try {
-            Thread.sleep(5000); // wait 5 seconds
+            Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
