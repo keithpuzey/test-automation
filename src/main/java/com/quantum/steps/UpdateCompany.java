1d2cce7481f07a0cf290e305e83929dfbb0f037e
keith Puzey <keithpuzey@users.noreply.github.com>
2025-08-26 18:03:30 +0100

    update test

--- src/main/java/com/quantum/steps/UpdateCompany.java
+++ src/main/java/com/quantum/steps/UpdateCompany.java
@@ -22,8 +22,9 @@ public class UpdateCompany {
            QAFWebDriver driver = new WebDriverTestBase().getDriver();
 
         driver.executeScript("perfecto:ai:user-action",
-            Map.of("action", "go to url https://perforce-dev-ed.develop.my.salesforce.com/ and login as the users salesexec@perforce.com with the password P4Demo123"));
-	
+            Map.of("action", "use a browser to go to url https://perforce-dev-ed.develop.my.salesforce.com/ "));
+            Map.of("action", "login as the users salesexec@perforce.com with the password P4Demo123");
+    		
     }
 
 
