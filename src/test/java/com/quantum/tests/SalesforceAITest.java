9138e285195c199ccbc6afe54f3e26dc350e7a4c
keith Puzey <keithpuzey@users.noreply.github.com>
2025-08-27 13:08:41 +0100

    add commas

--- src/test/java/com/quantum/tests/SalesforceAITest.java
+++ src/test/java/com/quantum/tests/SalesforceAITest.java
@@ -23,7 +23,7 @@ public class SalesforceAITest implements ITest {
         currentTestName = "TC56 - Search and Validate [" + accountName + "]";
 
         driver.executeScript("perfecto:ai:user-action",
-                Map.of("action", "use a browser to go to url https://perforce-dev-ed.develop.my.salesforce.com/ and login as the user salesexec@perforce.com with the password P4Demo123"));
+                Map.of("action", "use a browser to go to url https://perforce-dev-ed.develop.my.salesforce.com/ ,login as the user salesexec@perforce.com with the password P4Demo123, click login"));
   
     }
 
