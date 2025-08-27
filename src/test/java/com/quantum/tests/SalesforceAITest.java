b2dfe9c61400c7557966862de030ae3f98654148
keith Puzey <keithpuzey@users.noreply.github.com>
2025-08-27 10:29:02 +0100

    add csv file

--- src/test/java/com/quantum/tests/SalesforceAITest.java
+++ src/test/java/com/quantum/tests/SalesforceAITest.java
@@ -1,23 +1,39 @@
 package com.quantum.tests;
 
+import org.testng.ITest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import com.qmetry.qaf.automation.ui.WebDriverTestBase;
 import com.qmetry.qaf.automation.ui.webdriver.QAFWebDriver;
 import java.util.Map;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SalesforceAITest {
+public class SalesforceAITest implements ITest {
 
     private QAFWebDriver driver;
+    private String currentTestName;  // store dynamic test name
 
     @BeforeMethod
     public void initDriver() {
-        // Create a WebDriverTestBase and get the driver from it
         driver = new WebDriverTestBase().getDriver();
     }
 
-    @Test
-    public void loginSearchValidateCompany() {
+    @DataProvider(name = "accountProvider")
+    public Object[][] getAccounts() throws Exception {
+        Iterator<Object[]> csvData = CSVUtils.readCSV("src/test/resources/accounts.csv");
+        List<Object[]> list = new ArrayList<>();
+        csvData.forEachRemaining(list::add);
+        return list.toArray(new Object[0][]);
+    }
+
+    @Test(dataProvider = "accountProvider")
+    public void loginSearchValidateCompany(String accountName) {
+        // Set the dynamic test name for ITest
+        currentTestName = "TC56 - Search and Validate [" + accountName + "]";
+
         driver.executeScript("perfecto:ai:user-action",
                 Map.of("action", "use a browser to go to url https://perforce-dev-ed.develop.my.salesforce.com/"));
 
@@ -25,9 +41,14 @@ public class SalesforceAITest {
                 Map.of("action", "login as the user salesexec@perforce.com with the password P4Demo123"));
 
         driver.executeScript("perfecto:ai:user-action",
-                Map.of("action", "search for account Pyramid Construction Inc."));
+                Map.of("action", "search for account " + accountName));
 
         driver.executeScript("perfecto:ai:validation",
-                Map.of("validation", "Screen shows details for company Pyramid Construction Inc."));
+                Map.of("validation", "Screen shows details for company " + accountName));
+    }
+
+    @Override
+    public String getTestName() {
+        return currentTestName;  // TestNG uses this for JUnit / HTML report
     }
 }
