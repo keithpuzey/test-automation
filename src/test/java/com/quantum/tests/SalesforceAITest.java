d17eecd2ad57f18a687cc6cbbc5d5dcac9833fca
keith Puzey <keithpuzey@users.noreply.github.com>
2025-08-27 15:31:18 +0100

    move device selection to test

--- src/test/java/com/quantum/tests/SalesforceAITest.java
+++ src/test/java/com/quantum/tests/SalesforceAITest.java
@@ -1,10 +1,13 @@
 package com.quantum.tests;
 
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.ITest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import com.qmetry.qaf.automation.ui.WebDriverTestBase;
 import com.qmetry.qaf.automation.ui.webdriver.QAFWebDriver;
+import com.qmetry.qaf.automation.ui.WebDriverTestBase;
+
+import java.net.MalformedURLException;
 import java.util.Map;
 
 public class SalesforceAITest implements ITest {
@@ -13,8 +16,22 @@ public class SalesforceAITest implements ITest {
     private String currentTestName;
 
     @BeforeMethod
-    public void initDriver() {
-        driver = new WebDriverTestBase().getDriver();
+    public void initDriver() throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+
+        // Perfecto authentication
+        caps.setCapability("perfecto:user", "<your-user>");
+        caps.setCapability("perfecto:securityToken", "<your-token>");
+
+        // Device selection
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("appium:platformVersion", "14");  // appium: prefix is important in W3C
+        caps.setCapability("deviceName", ".*"); // regex for any device
+
+        // Browser
+        caps.setCapability("browserName", "Chrome");
+
+        driver = new WebDriverTestBase(caps).getDriver();
     }
 
     @Test
@@ -24,7 +41,6 @@ public class SalesforceAITest implements ITest {
 
         driver.executeScript("perfecto:ai:user-action",
                 Map.of("action", "login as the user salesexec@perforce.com with the password P4Demo123 using the url https://perforce-dev-ed.develop.my.salesforce.com/ "));
-  
     }
 
     @Override
