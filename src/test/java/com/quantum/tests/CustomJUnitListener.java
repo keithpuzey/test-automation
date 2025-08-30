7ca443fbdb1fe991ebf2d86875df41a80aed0eda
keithpuzey <keithpuzey@users.noreply.github.com>
2025-08-30 17:53:09 +0100

    Update CustomJUnitListener.java

--- src/test/java/com/quantum/tests/CustomJUnitListener.java
+++ src/test/java/com/quantum/tests/CustomJUnitListener.java
@@ -1,62 +1,24 @@
 package com.quantum.tests;
 
-import org.testng.IReporter;
-import org.testng.ISuite;
-import org.testng.ISuiteResult;
+import org.testng.IInvokedMethod;
+import org.testng.IInvokedMethodListener;
 import org.testng.ITestResult;
-import org.testng.xml.XmlSuite;
+import org.testng.Reporter;
 
-import java.io.FileWriter;
-import java.util.List;
-import java.util.Map;
-
-public class CustomJUnitListener implements IReporter {
+public class StepLoggingListener implements IInvokedMethodListener {
 
     @Override
-    public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
-        try (FileWriter writer = new FileWriter(outputDirectory + "/custom-junit-report.xml")) {
-            writer.write("<testsuites>\n");
-
-            for (ISuite suite : suites) {
-                Map<String, ISuiteResult> results = suite.getResults();
-                for (ISuiteResult sr : results.values()) {
-                    List<ITestResult> passedTests = sr.getTestContext().getPassedTests().getAllResults().stream().toList();
-                    List<ITestResult> failedTests = sr.getTestContext().getFailedTests().getAllResults().stream().toList();
-
-                    writeResults(writer, passedTests, "passed");
-                    writeResults(writer, failedTests, "failure");
-                }
-            }
-
-            writer.write("</testsuites>\n");
-            writer.flush();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void beforeInvocation(IInvokedMethod method, ITestResult testResult) {
+        // Not needed here
     }
 
-    private void writeResults(FileWriter writer, List<ITestResult> results, String status) throws Exception {
-        for (ITestResult tr : results) {
-            Object instance = tr.getInstance();
-            String testName = (instance instanceof org.testng.ITest) ?
-                    ((org.testng.ITest) instance).getTestName() : tr.getMethod().getMethodName();
-
-            writer.write(String.format(
-                    "<testcase classname=\"%s\" name=\"%s\" time=\"%.2f\">\n",
-                    tr.getTestClass().getName(),
-                    testName,
-                    (tr.getEndMillis() - tr.getStartMillis()) / 1000.0));
-
-            if ("failure".equals(status)) {
-                Throwable t = tr.getThrowable();
-                writer.write(String.format(
-                        "<failure message=\"%s\" type=\"%s\"><![CDATA[%s]]></failure>\n",
-                        t.getMessage(),
-                        t.getClass().getName(),
-                        t.toString()));
+    @Override
+    public void afterInvocation(IInvokedMethod method, ITestResult testResult) {
+        // If the method is a test method, flush Reporter logs to JUnit XML
+        if (method.isTestMethod()) {
+            for (String log : Reporter.getOutput(testResult)) {
+                testResult.setAttribute("step-log", log); // store as attribute
             }
-
-            writer.write("</testcase>\n");
         }
     }
 }
