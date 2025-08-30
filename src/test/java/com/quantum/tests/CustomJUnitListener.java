9333a5b0b95490cb04b40f9b83bb79874b33f3a8
keithpuzey <keithpuzey@users.noreply.github.com>
2025-08-30 18:11:58 +0100

    Update CustomJUnitListener.java

--- src/test/java/com/quantum/tests/CustomJUnitListener.java
+++ src/test/java/com/quantum/tests/CustomJUnitListener.java
@@ -1,24 +1,45 @@
 package com.quantum.tests;
 
-import org.testng.IInvokedMethod;
-import org.testng.IInvokedMethodListener;
 import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
 import org.testng.Reporter;
 
-public class StepLoggingListener implements IInvokedMethodListener {
+import java.util.List;
+
+public class CustomJUnitListener extends TestListenerAdapter {
 
     @Override
-    public void beforeInvocation(IInvokedMethod method, ITestResult testResult) {
-        // Not needed here
+    public void onTestSuccess(ITestResult tr) {
+        logStepDetails(tr);
+        super.onTestSuccess(tr);
     }
 
     @Override
-    public void afterInvocation(IInvokedMethod method, ITestResult testResult) {
-        // If the method is a test method, flush Reporter logs to JUnit XML
-        if (method.isTestMethod()) {
-            for (String log : Reporter.getOutput(testResult)) {
-                testResult.setAttribute("step-log", log); // store as attribute
+    public void onTestFailure(ITestResult tr) {
+        logStepDetails(tr);
+        super.onTestFailure(tr);
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult tr) {
+        logStepDetails(tr);
+        super.onTestSkipped(tr);
+    }
+
+    private void logStepDetails(ITestResult tr) {
+        List<String> logs = Reporter.getOutput(tr);
+        if (!logs.isEmpty()) {
+            StringBuilder sb = new StringBuilder("\n--- Step Logs ---\n");
+            for (String log : logs) {
+                sb.append(log).append("\n");
             }
+            sb.append("-----------------\n");
+
+            // Attach the step logs so they appear in the JUnit XML <system-out>
+            tr.setAttribute("step-logs", sb.toString());
+
+            // Also send to console
+            System.out.println(sb.toString());
         }
     }
 }
